7c74466242fdf417001ee8cad9dad15641d529d5
windr <imrwz@icloud.com>
2017-11-13 13:24:03 +0800

    use fwrite instead of fputc in loop

--- test/test.c
+++ test/test.c
@@ -4,8 +4,8 @@
 
 int main() {
     const char *path = "Z:\\tmp.fs";
-    int handler = init_fs("Z:\\tmp.fs", 131072 /*262144*/, FS_LAZY);
-    printf("created file system at %s\n with handler %d", path, handler);
+    int handler = init_fs("Z:\\tmp.fs", 131072 /*262144*/, FS_ONCE);
+    printf("created file system at %s with handler %d\n", path, handler);
     close_fs(handler);
     return 0;
 }
