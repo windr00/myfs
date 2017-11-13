01cfb1614ba3031e067b2ac0b3a79f0765f83940
windr <imrwz@icloud.com>
2017-11-13 13:13:47 +0800

    use fwrite instead of fputc in loop

--- imp/fsop.c
+++ imp/fsop.c
@@ -38,27 +38,24 @@ int find_empty_block(fs *file) {
     }
 }
 
-int expand_disk_size(fs *file, long block_offset) {
+int expand_disk_size(fs *file, long block_index) {
     fseek(file->mem_fd, file->mem_blockSectorOffset, 0);
-    for (int i = 0; i < block_offset; i++) {
-        if (fputc(0, file->mem_fd) == EOF) {
-            return VIRTUAL_DISK_FILE_WRITE_ERROR;
-        }
+    char block[BLOCK_SIZE_IN_BYTE] = {0};
+    for (int i = 0; i < block_index; i++) {
+        fwrite(block, BLOCK_SIZE_IN_BYTE, 1, file->mem_fd);
     }
     return VIRTUAL_DISK_OPERATION_SUCCESS;
 }
 
 int write_block(fs *file, long size, void *memory, long blockIndex) {
     if (file->disk_mode == FS_LAZY) {
-        int ret = expand_disk_size(file, blockIndex * BLOCK_SIZE_IN_BYTE);
+        int ret = expand_disk_size(file, blockIndex);
         if (ret != VIRTUAL_DISK_OPERATION_SUCCESS) {
             return ret;
         }
     }
     fseek(file->mem_fd, file->mem_blockSectorOffset + blockIndex * BLOCK_SIZE_IN_BYTE, 0);
-    for (int i = 0; i < size; i++) {
-        fputc(*((unsigned char *) (memory + i)), file->mem_fd);
-    }
+    fwrite(memory, sizeof(unsigned char), (size_t) size, file->mem_fd);
     file->disk_bitMap[blockIndex] = BLOCK_FLAG_TAKEN;
     return VIRTUAL_DISK_OPERATION_SUCCESS;
 }
@@ -69,12 +66,10 @@ fsd_t load_fs(const char *fspath) {
 
 int write_inode(fs *file, inode *cache, int validInodeCount) {
     fseek(file->mem_fd, file->mem_inodeSectorOffset, 0);
-    for (int i = 0; i < validInodeCount; i++) {
-        for (int j = 0; j < INODE_SIZE_IN_BYTE; j++) {
-            if (fputc(*((char *) cache + j), file->mem_fd) == EOF) {
-                return VIRTUAL_DISK_FILE_WRITE_ERROR;
-            }
-        }
+    fwrite(cache, INODE_SIZE_IN_BYTE, (size_t) validInodeCount, file->mem_fd);
+    char tmp[INODE_SIZE_IN_BYTE] = {0};
+    for (int i = 0; i < file->disk_blockCount - validInodeCount; i++) {
+        fwrite(tmp, INODE_SIZE_IN_BYTE, 1, file->mem_fd);
     }
     return VIRTUAL_DISK_OPERATION_SUCCESS;
 }
@@ -83,18 +78,10 @@ int write_fs_info(fs *newFs) {
     fseek(newFs->mem_fd, 0, 0);
     fprintf(newFs->mem_fd, "%ld%ld", newFs->disk_blockCount, newFs->disk_validInodeCount);
     newFs->mem_bitmapOffset = ftell(newFs->mem_fd);
-    for (long i = 0; i < newFs->disk_blockCount; i++) {
-        fprintf(newFs->mem_fd, "%c", newFs->disk_bitMap[i]);
-    }
+    fwrite(newFs->disk_bitMap, sizeof(char), (size_t) newFs->disk_blockCount, newFs->mem_fd);
     newFs->mem_inodeSectorOffset = ftell(newFs->mem_fd);
     write_inode(newFs, newFs->disk_inodeCache, newFs->disk_validInodeCount);
-    for (long i = 0; i < newFs->disk_blockCount * INODE_SIZE_IN_BYTE; i++) {
-        if (fputc(0, newFs->mem_fd) == EOF) {
-            fclose(newFs->mem_fd);
-            free(newFs);
-            return VIRTUAL_DISK_FILE_WRITE_ERROR;
-        }
-    }
+    fseek(newFs->mem_fd, newFs->disk_blockCount * INODE_SIZE_IN_BYTE, newFs->mem_inodeSectorOffset);
     newFs->mem_blockSectorOffset = ftell(newFs->mem_fd);
     return VIRTUAL_DISK_OPERATION_SUCCESS;
 }
@@ -108,19 +95,14 @@ fsd_t init_fs(const char *fspath, long blockCount, int mode) {
     }
     newFs->disk_mode = mode;
     newFs->mem_diskFileName = fspath;
-    newFs->disk_bitMap = (unsigned char *) malloc(blockCount * BLOCK_SIZE_IN_BYTE);
-    memset(newFs->disk_bitMap, BLOCK_FLAG_EMPTY, blockCount * BLOCK_SIZE_IN_BYTE);
+    newFs->disk_bitMap = (unsigned char *) malloc(blockCount);
+    memset(newFs->disk_bitMap, BLOCK_FLAG_EMPTY, blockCount);
     newFs->mem_operatorList = NULL;
     write_fs_info(newFs);
     if (mode == FS_ONCE) {
+        char block[BLOCK_SIZE_IN_BYTE] = {0};
         for (long i = 0; i < blockCount; i++) {
-            for (long j = 0; j < BLOCK_SIZE_IN_BYTE; j++) {
-                if (fputc(0, newFs->mem_fd) == EOF) {
-                    fclose(newFs->mem_fd);
-                    free(newFs);
-                    return VIRTUAL_DISK_FILE_WRITE_ERROR;
-                }
-            }
+            fwrite(block, BLOCK_SIZE_IN_BYTE, 1, newFs->mem_fd);
         }
     }
     dirdata data;
@@ -155,7 +137,6 @@ fsd_t init_fs(const char *fspath, long blockCount, int mode) {
 int close_fs(fsd_t fsd) {
     fs *file = openedFileSystem + fsd;
     int ret = write_fs_info(file);
-    ret &= write_inode(file, file->disk_inodeCache, file->disk_validInodeCount);
     ret &= fclose(file->mem_fd);
     if (ret) {
         free(&openedFileSystem[fsd]);
--- test/test.c
+++ test/test.c
@@ -4,7 +4,7 @@
 
 int main() {
     const char *path = "Z:\\tmp.fs";
-    int handler = init_fs("Z:\\tmp.fs", 100, FS_ONCE);
+    int handler = init_fs("Z:\\tmp.fs", 131072 /*262144*/, FS_LAZY);
     printf("created file system at %s\n with handler %d", path, handler);
     close_fs(handler);
     return 0;
